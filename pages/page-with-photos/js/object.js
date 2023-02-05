db14b0cc23fd93b3a52edace7eeb98291eaf1827
Elena <ticauelena5@gmail.com>
2023-02-05 15:36:54 +0200

    commit object

---
+++ pages/page-with-photos/js/object.js
@@ -0,0 +1,8 @@
+var student = {};
+student.firstName = 'John';
+student.lastName = 'Smith';
+student.averageScore = 8.7;
+
+console.log("First name: " + student.firstName);
+console.log("Last name: " + student.lastName);
+console.log("Average score: " + student.averageScore);
